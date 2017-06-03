e732c0a004a3c54d2169e8eaac4c5a0ae243d2a6
Chad Harrington <chad@farbetter.com>
2017-06-02 19:49:00 -0600

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deercreeklabs/stockroom "0.1.10"
+(defproject deercreeklabs/stockroom "0.1.11-SNAPSHOT"
   :description "Clojure/Clojurescript cache, using the Clock algorithm."
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"

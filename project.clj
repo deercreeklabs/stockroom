ecc06a54704e3caad6d9094db7ef5f27b72b981c
Chad Harrington <chad@deercreeklabs.com>
2018-12-29 20:19:04 -0700

    lein-release plugin: bumped version from 0.1.18 to 0.1.19-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/stockroom "0.1.18"
+(defproject deercreeklabs/stockroom "0.1.19-SNAPSHOT"
   :description "Clojure/Clojurescript cache, using the Clock algorithm."
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
